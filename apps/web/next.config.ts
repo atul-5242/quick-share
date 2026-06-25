71d748f20147c1cf84b45515aaf32bce138e9528
Atul Maurya <atul.maurya@capgemini.com>
2026-06-25 09:14:42 +0530

    Some required #sh changes done.

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ["10.110.166.168"],
+  allowedDevOrigins: ["10.110.166.168","10.110.167.127"],
 };
 
 export default nextConfig;
